b821fa6bf285e8169157b31f1bab57858ba8c33a
beardad1975 <beardad1975@nmes.tyc.edu.tw>
2022-04-05 21:14:12 +0800

    bumped version 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="threed4t",
-    version="0.0.3",
+    version="0.0.4",
     author="Wen-Hung, Chang 張文宏",
     author_email="beardad1975@nmes.tyc.edu.tw",
     description="3d learning module for Teenagers",
